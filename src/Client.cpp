7c5c45b1e43c2fc2f00117ed2bfe6455bc6c0cdb
anythingtechpro <anythingtechpro@gmail.com>
2018-09-10 16:36:54 -0400

    fixed accidental merge with another file when converting tabs to spaces

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,22 +1,4 @@
-#ifndef UTILS_H_
-#define UTILS_H_
-
-#include <cstdint>
-
-#include <string>
-
-namespace Utils {
-
-// http://www.cplusplus.com/reference/ctime/strftime/
-// https://cppcodetips.wordpress.com/2014/01/02/a-simple-logger-class-in-c/
-const std::string CurrentDate();
-const std::string CurrentTime();
-
-int32_t Distance3d(int32_t x1, int32_t y1, int32_t z1, int32_t x2, int32_t y2, int32_t z2);
-bool DistanceCheck(int32_t distance, int32_t x1, int32_t y1, int32_t z1, int32_t x2, int32_t y2, int32_t z2);
-
-unsigned int GetRandomUInt(unsigned int n);
-std::string GetRandomSalt();#include "Client.hpp"
+#include "Client.hpp"
 #include "World.hpp"
 
 #include "Utils/Logger.hpp"
@@ -113,8 +95,3 @@ void Client::ProcessPacketsInQueue()
     it = m_packetQueue.erase(it);
   }
 }
-
-
-} // namespace Utils
-
-#endif // UTILS_H_
